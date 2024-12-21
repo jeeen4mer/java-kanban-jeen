ffe8d28e4a9eec7d895ce74e9e3d332b1c3aa364
JeeN4MeR <ibakulev04@gmail.com>
2024-12-21 19:59:38 +0400

    Fixed checks 5

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -25,7 +25,7 @@ public class Epic extends Task {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Epic epic = (Epic) o;
-        return Objects.equals(id, epic.id) && Objects.equals(name, epic.name) && Objects.equals(description, epic.description) && taskStatus == epic.taskStatus ;
+        return Objects.equals(id, epic.id) && Objects.equals(name, epic.name) && Objects.equals(description, epic.description) && taskStatus == epic.taskStatus;
     }
 
     @Override
