d65be64f9fe9e7f87d6c39f7cebd99102b060e41
JeeN4MeR <ibakulev04@gmail.com>
2024-12-21 21:10:36 +0400

    Fixed checks 17

--- src/manager/InMemoryTasksManager.java
+++ src/manager/InMemoryTasksManager.java
@@ -127,8 +127,7 @@ public class InMemoryTasksManager implements TaskManager {
             epic.setTaskStatus(TaskStatus.DONE);
         } else if (hasInProgressSubtasks) {
             epic.setTaskStatus(TaskStatus.IN_PROGRESS);
-        }
-        else {
+        } else {
             epic.setTaskStatus(TaskStatus.NEW);
         }
     }
