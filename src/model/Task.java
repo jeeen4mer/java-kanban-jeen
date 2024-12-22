710ea1ddff6abb9b5c48f5c929be5a19d08f5ac3
JeeN4MeR <ibakulev04@gmail.com>
2024-12-22 14:45:26 +0400

    Fixed with comments 4

--- src/model/Task.java
+++ src/model/Task.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.Objects;
 
-public class Task extends Node {
+public class Task {
 
     protected Integer id;
     protected String name;
@@ -11,7 +11,6 @@ public class Task extends Node {
 
 
     public Task(String name, String description) {
-        super(name, description);
         this.name = name;
         this.description = description;
     }
